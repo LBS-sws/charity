d82a8f23cad69a78e552e55d702e4265415836cf
LBS IT <percylee@lbsgroup.com.hk>
2020-10-08 11:30:27 +0800

    英文顯示

---
+++ protected/messages/en/app.php
@@ -0,0 +1,109 @@
+<?php
+return array(
+/*
+	'Daily Report System'=>'日报表系统',
+	'Daily Report System - UAT'=>'日报表系统 - 测试版',
+	'LBS Daily Management'=>'LBS 日常管理',
+	'LBS Daily Management - UAT'=>'LBS 日常管理 - 测试版',
+	'LBS DMS (UAT)'=>'LBS 日常管理 (测试)',
+	'LBS DMS'=>'LBS 日常管理',
+	'MAIN NAVIGATION'=>'系统菜单',
+	'Region'=>'地区',
+	'Last Update Date '=>'更新日期 ',
+
+	'System Change'=>'系统切换',
+	'Daily Report'=>'日报表系统',
+	'Accounting'=>'会计系统',
+	'Operation'=>'营运系统',
+	'Personnel'=>'人事系统',
+        'Sales'=>'销售系统',
+    'Quiz'=>'培训测验系统',
+	'Academic Credit'=>'学分系统',
+    'Integral'=>'学分系统',
+    'Charitable Credit'=>'慈善分系统',
+	'Online Library'=>'网上文库',
+	'System Use'=>'系统使用',
+
+
+	'Data Entry'=>'数据输入',
+	'Misc'=>'其他',
+
+	'Staff Info'=>'员工资料',
+	'Employer'=>'雇主资料',
+	'Department'=>'工作部门',
+	'Leader'=>'工作职位',
+
+	'Report'=>'报表',
+	'Report Manager'=>'报表管理员',
+
+	'System Setting'=>'系统设置',
+	'Security'=>'保安',
+	'Tools' => '工具',
+	
+	'Change Password' => '更改密码',
+	'Languages' => '语言',
+	
+	'Checker Right'=>'会计对账权限',
+	'Transaction Adjustment'=>'交易调整权限',
+    'Modify Restrict'=>'修改文档限制',
+	
+	'User'=>'帐户',
+	'Group'=>'群组',
+	'Access Template'=>'权限范本',
+
+	'New'=>'新增',
+	'Renew'=>'续约',
+	'Resume'=>'恢复',
+	'Amend'=>'更改',
+	'Suspend'=>'暂停',
+	'Terminate'=>'终止',
+	'(Resign)'=>'（离职）',
+	
+	'Active' => '启用',
+	'Inactive' => '停用',
+	'already used'=>'已被取用',
+	'Void'=>'撤消',
+	'Normal'=>'正常',
+	
+	'Pending'=>'等候',
+	'In Progress'=>'进行中',
+	'Complete'=>'完成',
+	'Fail'=>'失败',
+	'Sent'=>'送出',
+	
+	'Last Logon Time'=>'最近登录时间',
+	'Others'=>'其他',
+
+	'Account Type'=>'账户类别',
+	'Transaction Type'=>'交易类别',
+	'Account'=>'账户',
+
+    //沈超添加开始
+    'Audit'=>'审核',
+    'Charity Credit'=>'慈善分',
+    'Exchange'=>'积分',
+    'Search'=>'查询',
+
+    'Apply Charity Credit'=>'申请慈善分',
+    'Apply Charity Prize'=>'兑换慈善分',
+
+    'Audit Charity Credit'=>'审核慈善分',
+    'Audit Charity Prize'=>'审核慈善分兑换',
+
+    'Charity Credit Search'=>'慈善分查詢',
+    'Charity Credit Search Sum'=>'总慈善分查詢',
+    'Charity Prize Search'=>'慈善分兑换查詢',
+
+    'Charity Credit Report'=>'慈善分报表',
+    'Charity Prize Report'=>'慈善分兑换报表',
+
+    'Charity Credit type'=>'慈善分配置',
+    'Charity Prize type'=>'兑换配置',
+
+    'Cancel the Charity'=>'取消已审核的慈善分',
+    'Cancel the Prize'=>'取消已审核的兑换',
+    'Search All City'=>'查询所有城市',
+    //沈超添加结束
+*/
+);
+?>
---
+++ protected/messages/en/report.php
@@ -0,0 +1,40 @@
+<?php
+return array(
+//	'City'=>'地区',
+//	'Type'=>'类别',
+//	'Year'=>'年',
+//	'Month'=>'月',
+//	'Nil'=>'无资料',
+//	'Page'=>'页',
+//	'Date'=>'日期',
+//	'Start Date'=>'开始日期',
+//	'End Date'=>'结束日期',
+//	'Dear All'=>'各位',
+	
+//	'Staff List'=>'员工名册',
+
+//	'Please find the attached report for your reference.'=>'请查看附件中的报告以供参考.',
+//	'Name'=>'姓名',
+//	'Gender'=>'性别',
+//	'Position'=>'职位',
+//	'ID No.'=>'公民身份号码',
+//	'ID Valid Date'=>'公民身份证有效期',
+//	'DOB'=>'出生日期',
+//	'Original Address'=>'户籍地址',
+//	'Contact Address'=>'现住址',
+//	'Contact Type'=>'联系方式（如手机号）',
+//	'Contract Type'=>'用工形式',
+//	'Join Date'=>'用工（上班）起始日期',
+//	'Contract Start Date'=>'劳动合同起始日期',
+//	'Contract End Date'=>'劳动合同终止日期',
+//	'Social Security Code'=>'社保卡号',
+//	'Emergency Contact'=>'紧急联络人姓名',
+//	'Emergency Phone'=>'紧急联络人手机号',
+//	'Leave/Change Date'=>'离职/调职日期',
+//	'Leave/Change Reason'=>'离职/调职原因',
+//	'Remarks'=>'备注',
+//	'Staffs'=>'员工',
+//	'Staff No.'=>'员工编号',
+
+);
+?>
