5796403540358bcb4a85d9b13c744a3f1940c89b
LBS IT <shenchao@lbsgroup.com.cn>
2021-03-15 19:38:30 +0800

    慈善分菜单添加角标

--- protected/config/notifybadge.php
+++ protected/config/notifybadge.php
@@ -1,13 +1,10 @@
 <?php
 
 return array(
-	array('code'=>'YS01','function'=>'Counter::getImdoNum','color'=>'bg-green'),
-	array('code'=>'YS04','function'=>'Counter::getFastNum','color'=>'bg-yellow'),
-	array('code'=>'YD02','function'=>'Counter::getDeliNum','color'=>'bg-maroon'),
-	array('code'=>'YD03','function'=>'Counter::getTakeNum','color'=>'bg-light-blue'),
-	array('code'=>'YD06','function'=>'Counter::getAreaNum','color'=>'bg-purple'),
-	array('code'=>'YC02','function'=>'Counter::getGoodsNum','color'=>'bg-gray'),
-	array('code'=>'YA03','function'=>'Counter::getRepNum','color'=>'bg-maroon'),
+    array('code'=>'CY01','function'=>'Counter::getApplyCredit','color'=>'bg-yellow'),
+	array('code'=>'CY02','function'=>'Counter::getApplyPrize','color'=>'bg-green'),
+	array('code'=>'GA01','function'=>'Counter::getAuditCredit','color'=>'bg-maroon'),
+	array('code'=>'GA02','function'=>'Counter::getAuditPrize','color'=>'bg-light-blue'),
 );
 
 ?>
