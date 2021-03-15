aa8fb37fd1776302736f7bd7c7414f6d7e060e6a
LBS IT <shenchao@lbsgroup.com.cn>
2021-03-15 20:29:53 +0800

    修改审核的角标

--- protected/models/Counter.php
+++ protected/models/Counter.php
@@ -25,20 +25,20 @@ class Counter {
 	//申请慈善分审核
 	public static function getAuditCredit() {
         $suffix = Yii::app()->params['envSuffix'];
-        $staffId = Yii::app()->user->staff_id();//
+        $city_allow = Yii::app()->user->city_allow();
         $imDo_num = Yii::app()->db->createCommand()->select("count(a.id)")->from("cy_credit_request a")
             ->leftJoin("hr$suffix.hr_employee d","a.employee_id = d.id")
-            ->where('state=1 and d.id=:id',array(":id"=>$staffId))->queryScalar();
+            ->where("d.city IN ($city_allow) AND a.state = 1")->queryScalar();
         return $imDo_num;
 	}
 
 	//慈善分兑换审核
     public static function  getAuditPrize(){
         $suffix = Yii::app()->params['envSuffix'];
-        $staffId = Yii::app()->user->staff_id();//
+        $city_allow = Yii::app()->user->city_allow();
         $imDo_num = Yii::app()->db->createCommand()->select("count(a.id)")->from("cy_prize_request a")
             ->leftJoin("hr$suffix.hr_employee d","a.employee_id = d.id")
-            ->where('state=1 and d.id=:id',array(":id"=>$staffId))->queryScalar();
+            ->where("d.city IN ($city_allow) AND a.state = 1")->queryScalar();
         return $imDo_num;
     }
 
