0f139ba6ad4e7754ce54303924683d35f68678da
shenchao <shenchao@lbsgroup.com.cn>
2021-08-17 16:15:49 +0800

    測驗系統的彈窗提示

--- protected/messages/en/block.php
+++ protected/messages/en/block.php
@@ -11,5 +11,8 @@ return array(
 	'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
 	=>'在使用其他功能前，请先完成学分系统-学分专员确认。',
 */
+    'validateExamination'=>'The average score of customer service last month was less than 75. Please log in the test system to do the test<br/>Requirement: 85% correct answer rate, otherwise you need to continue the test',
+    'validateNewStaff'=>'Welcome to Join Swisher to ensure service quality<br>Please complete the first training test within 3 months from the date of commencement<br/>Requirement: 85% correct answer rate, otherwise you need to continue the test',
+    'EveryYearForExamination'=>'Dear colleagues, in order to consolidate the technical service knowledge and service quality<br/> please complete the annual training test by the end of next month',
 );
 ?>
