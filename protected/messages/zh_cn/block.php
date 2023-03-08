c77a984aa15e1d5ac424ae4e2eb13ac46aae87a2
shenchao <shenchao@lbsgroup.com.cn>
2023-03-08 11:26:41 +0800

    sysblock翻譯文件

--- protected/messages/zh_cn/block.php
+++ protected/messages/zh_cn/block.php
@@ -1,17 +1,17 @@
 <?php
-return array(
-	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。',
-	'Please complete Operation System - Sales Summary Report Approval before using other functions.'
-	=>'在使用其他功能前，请先完成营运系统-营业报告审核。',
-	'Please complete Operation System - Sales Summary Report Submission before using other functions.'
-	=>'在使用其他功能前，请先完成营运系统-营业报告提交。',
-	'Please complete Academic Credit System - Credit Request Approval before using other functions.'
-	=>'在使用其他功能前，请先完成学分系统-学分审核。',
-	'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
-	=>'在使用其他功能前，请先完成学分系统-学分专员确认。',
+return array( 
+    'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。',
+    'Please complete Operation System - Sales Summary Report Approval before using other functions.'
+    =>'在使用其他功能前，请先完成营运系统-营业报告审核。',
+    'Please complete Operation System - Sales Summary Report Submission before using other functions.'
+    =>'在使用其他功能前，请先完成营运系统-营业报告提交。',
+    'Please complete Academic Credit System - Credit Request Approval before using other functions.'
+    =>'在使用其他功能前，请先完成学分系统-学分审核。',
+    'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
+    =>'在使用其他功能前，请先完成学分系统-学分专员确认。',
     'validateExamination'=>'上月服务客户质检出的平均分低于75分，请登录测验系统进行测验做题<br/>要求：答题正确率为85%, 否则需继续测验',
     'validateNewStaff'=>'欢迎加入史伟莎, 为确保服务品质<br>请于到职日起算三个月内, 完成第一次的培训测验<br/>要求：答题正确率为85%, 否则需继续测验',
-	'Please complete Report System - Sales Summary Report Submission before using other functions.'
+    'Please complete Report System - Sales Summary Report Submission before using other functions.'
     =>'在使用其他功能前，请先完成日报表系统-月报表总结提交。',
     'Please complete Personnel System - Pennants Audit before using other functions.'
     =>'在使用其他功能前，请先完成人事系统-锦旗审核。',
