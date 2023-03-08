f39cab9abf463796c9c7f320fb1ea2e457778f8d
shenchao <shenchao@lbsgroup.com.cn>
2023-03-08 14:02:59 +0800

    SysBlock功能統一調用日報表文件

--- protected/components/General.php
+++ protected/components/General.php
@@ -50,6 +50,22 @@ class General extends CGeneral {
             ->from("hr$suffix.hr_setting")->where("set_name='systemId'")->queryScalar();
         return $value?$value:0;
     }
+
+    /*
+     * 加载日报表系统的SysBlock文件
+     * @return str
+     */
+    public static function includeDrsSysBlock(){
+        $systemList = require(Yii::app()->basePath.'/config/system.php');
+        foreach ($systemList as $row){
+            if($row["name"]=="Daily Report"){//读取日报表系统的公共文件
+                $objName = end(explode("/",$row["webroot"]));
+                $configPath = dirname(Yii::app()->basePath)."/../{$objName}/protected";
+                include_once($configPath."/components/SysBlock.php");
+                return true;
+            }
+        }
+    }
 }
 
 ?>
