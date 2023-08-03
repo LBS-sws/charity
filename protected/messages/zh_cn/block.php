e2f1fc3dea9da32a03ccf8dceaa70f702c11b1b3
shenchao <shenchao@lbsgroup.com.cn>
2023-08-03 11:13:03 +0800

    招聘彈窗提示文字

--- protected/messages/zh_cn/block.php
+++ protected/messages/zh_cn/block.php
@@ -1,17 +1,17 @@
 <?php
-return array( 
-    'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。',
-    'Please complete Operation System - Sales Summary Report Approval before using other functions.'
-    =>'在使用其他功能前，请先完成营运系统-营业报告审核。',
-    'Please complete Operation System - Sales Summary Report Submission before using other functions.'
-    =>'在使用其他功能前，请先完成营运系统-营业报告提交。',
-    'Please complete Academic Credit System - Credit Request Approval before using other functions.'
-    =>'在使用其他功能前，请先完成学分系统-学分审核。',
-    'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
-    =>'在使用其他功能前，请先完成学分系统-学分专员确认。',
+return array(
+	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。',
+	'Please complete Operation System - Sales Summary Report Approval before using other functions.'
+	=>'在使用其他功能前，请先完成营运系统-营业报告审核。',
+	'Please complete Operation System - Sales Summary Report Submission before using other functions.'
+	=>'在使用其他功能前，请先完成营运系统-营业报告提交。',
+	'Please complete Academic Credit System - Credit Request Approval before using other functions.'
+	=>'在使用其他功能前，请先完成学分系统-学分审核。',
+	'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
+	=>'在使用其他功能前，请先完成学分系统-学分专员确认。',
     'validateExamination'=>'上月服务客户质检出的平均分低于75分，请登录测验系统进行测验做题<br/>要求：答题正确率为85%, 否则需继续测验',
     'validateNewStaff'=>'欢迎加入史伟莎, 为确保服务品质<br>请于到职日起算三个月内, 完成第一次的培训测验<br/>要求：答题正确率为85%, 否则需继续测验',
-    'Please complete Report System - Sales Summary Report Submission before using other functions.'
+	'Please complete Report System - Sales Summary Report Submission before using other functions.'
     =>'在使用其他功能前，请先完成日报表系统-月报表总结提交。',
     'Please complete Personnel System - Pennants Audit before using other functions.'
     =>'在使用其他功能前，请先完成人事系统-锦旗审核。',
@@ -20,5 +20,7 @@ return array(
     'Please complete Charity System - Audit Charity Credit Confirmation before using other functions.'
     =>'在使用其他功能前，请先完成慈善分系统-慈善分专员审核。',
     'EveryYearForExamination'=>'亲爱的同事，为巩固技术服务知识和服务品质,请于次月底完成一年一次的培训测验<br/>要求：答题正确率为85%, 否则需继续测验',
+    'Please update the regional recruitment plan if there is any change. If it has been updated, ignore this information.'
+    =>'如地区招聘计划有变动，请及时更新。如已更新，请忽略此信息。',
 );
 ?>
--- protected/messages/zh_tw/block.php
+++ protected/messages/zh_tw/block.php
@@ -1,17 +1,17 @@
 <?php
-return array( 
-    'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，請先完成人事系統-優化人才評核。',
-    'Please complete Operation System - Sales Summary Report Approval before using other functions.'
-    =>'在使用其他功能前，請先完成營運系統-營業報告審核。 ',
-    'Please complete Operation System - Sales Summary Report Submission before using other functions.'
-    =>'在使用其他功能前，請先完成營運系統-營業報告提交。 ',
-    'Please complete Academic Credit System - Credit Request Approval before using other functions.'
-    =>'在使用其他功能前，請先完成學分系統-學分審核。 ',
-    'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
-    =>'在使用其他功能前，請先完成學分系統-學分專員確認。 ',
+return array(
+	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，請先完成人事系統-優化人才評核。',
+	'Please complete Operation System - Sales Summary Report Approval before using other functions.'
+	=>'在使用其他功能前，請先完成營運系統-營業報告審核。 ',
+	'Please complete Operation System - Sales Summary Report Submission before using other functions.'
+	=>'在使用其他功能前，請先完成營運系統-營業報告提交。 ',
+	'Please complete Academic Credit System - Credit Request Approval before using other functions.'
+	=>'在使用其他功能前，請先完成學分系統-學分審核。 ',
+	'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
+	=>'在使用其他功能前，請先完成學分系統-學分專員確認。 ',
     'validateExamination'=>'上月服務客戶質檢出的平均分低於75分，請登錄測驗系統進行測驗做題<br/>要求：答題正確率為85%, 否則需繼續測驗',
     'validateNewStaff'=>'歡迎加入史偉莎, 為確保服務品質<br>請於到職日起算三個月內, 完成第一次的培訓測驗<br/>要求：答题正确率为85%, 否则需继续测验',
-    'Please complete Report System - Sales Summary Report Submission before using other functions.'
+   'Please complete Report System - Sales Summary Report Submission before using other functions.'
     =>'在使用其他功能前，請先完成日报表系统-月报表总结提交。',
     'Please complete Personnel System - Pennants Audit before using other functions.'
     =>'在使用其他功能前，請先完成人事系統-錦旗審核。',
@@ -20,5 +20,7 @@ return array(
     'Please complete Charity System - Audit Charity Credit Confirmation before using other functions.'
     =>'在使用其他功能前，請先完成慈善分系統-慈善分專員審核。',
     'EveryYearForExamination'=>'親愛的同事，為鞏固技術服務只是和服務品質,請於次月底完成一年一次的培訓測驗<br/>要求：答題正確率為85%, 否則需繼續測驗',
+    'Please update the regional recruitment plan if there is any change. If it has been updated, ignore this information.'
+    =>'如地區招聘計劃有變動，請及時更新。如已更新，請忽略此信息。',
 );
 ?>
